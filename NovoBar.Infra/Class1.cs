295a6a868dc6f83a3bef664457f3d0cff66de589
RuiJ72 <rlagos900@gmail.com>
2025-03-02 12:25:35 -0300

    Definig apllication stucture and first configurations

---
+++ NovoBar.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NovoBar.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ NovoNar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NovoNar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
